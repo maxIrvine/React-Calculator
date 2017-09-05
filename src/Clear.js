e1fd193f74ab36a5f1cc67d67ea883a5e02ec172
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-05 14:04:53 -0400

    adds clear function

---
+++ src/Clear.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Calculator from './Calculator';
+
+let Clear = ({clickHandler}) => (
+    <button onClick={clickHandler}>Clear</button>
+)
+
+export default Clear;

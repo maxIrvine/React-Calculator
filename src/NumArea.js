9292dcbcd26b4db2da89f200fdd8486e271cd6fc
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-05 14:05:26 -0400

    adds numArea component to show number buttons

---
+++ src/NumArea.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Calculator from './Calculator';
+
+let NumArea = ({clickHandler}) => (
+    <div>
+        <div>
+            <button onClick={clickHandler(7)}>7</button>
+            <button onClick={clickHandler(8)}>8</button>
+            <button onClick={clickHandler(9)}>9</button>
+        </div>
+        <div>
+            <button onClick={clickHandler(4)}>4</button>
+            <button onClick={clickHandler(5)}>5</button>
+            <button onClick={clickHandler(6)}>6</button>
+        </div>
+        <div>
+            <button onClick={clickHandler(1)}>1</button>
+            <button onClick={clickHandler(2)}>2</button>
+            <button onClick={clickHandler(3)}>3</button>
+        </div>
+        <div>
+            <button onClick={clickHandler(0)}>0</button>
+            <button>.</button>
+        </div>
+    </div>
+)
+
+export default NumArea;

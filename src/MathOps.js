be001c8fb1d887fd857530c3b7384954314f311e
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-05 14:08:19 -0400

    adds mathOps component with various math operation buttons

---
+++ src/MathOps.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calculator from './Calculator';
+
+let MathOps = () => (
+    <div>
+        <button>/</button>
+        <button>*</button>
+        <button>-</button>
+        <button>+</button>
+        <button>=</button>
+    </div>
+)
+
+export default MathOps;

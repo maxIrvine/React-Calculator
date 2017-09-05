2e3d3fb15142b65ae7d1393346764fa7f27de407
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-05 15:42:30 -0400

    adjusts how the numbers are displayed

--- src/Display.js
+++ src/Display.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 let Display = ({num}) => {
     var newNum = num;
-    if (num === null) {
+    if (num === "") {
         newNum = 0;
-    } else {
-        newNum = newNum.substring(('null').length);
-        console.log(typeof newNum);
-    }
+    } 
     return (
         <div>{newNum}</div>
     )

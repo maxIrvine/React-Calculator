5bd55a105667b8dd1290b3ca9ca2ffd6573bd1c0
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-05 14:08:34 -0400

    updates calc components to receive proper parameters

---
+++ src/Calculator.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import Display from './Display';
+import Clear from './Clear';
+import NumArea from './NumArea';
+import MathOps from './MathOps';
+
+class Calculator extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            displayNumber: null
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <Display num={this.state.displayNumber}/>
+                <Clear clickHandler={this._clear}/>
+                <NumArea clickHandler={this._changeDisplay}/>
+                <MathOps />
+            </div>
+        );
+    }
+
+    _clear = () => {
+        this.setState({
+            displayNumber: 0
+        });
+    }
+
+    _changeDisplay = ({num}) => {
+        this.setState({
+            displayNumber: this.state.displayNumber + num
+        });
+    }
+}
+
+
+export default Calculator;

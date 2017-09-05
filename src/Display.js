46c9a8e4e44acd2031222ec5423b977ce8fa0ff7
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-05 15:26:42 -0400

    adds logic to clean up number to display

--- src/Display.js
+++ src/Display.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import Calculator from './Calculator';
 
-let Display = ({num}) => (
-    <div>{num}</div>
-)
+let Display = ({num}) => {
+    var newNum = num;
+    if (num === null) {
+        newNum = 0;
+    } else {
+        newNum = newNum.substring(('null').length);
+        console.log(typeof newNum);
+    }
+    return (
+        <div>{newNum}</div>
+    )
+}
 
 export default Display;

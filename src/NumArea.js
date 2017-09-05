646ca3161ffe854a31be309408fea3545297098f
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-05 15:42:12 -0400

    makes numbers concat

--- src/NumArea.js
+++ src/NumArea.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import Calculator from './Calculator';
 
 let NumArea = ({clickHandler}) => (
     <div>
         <div>
-            <button onClick={clickHandler(7)}>7</button>
-            <button onClick={clickHandler(8)}>8</button>
-            <button onClick={clickHandler(9)}>9</button>
+            <button onClick={() => {
+                clickHandler(7)    
+            }}>7</button>
+            <button onClick={() => {
+                clickHandler(8)    
+            }}>8</button>
+            <button onClick={() => {
+                clickHandler(9)    
+            }}>9</button>
         </div>
         <div>
-            <button onClick={clickHandler(4)}>4</button>
-            <button onClick={clickHandler(5)}>5</button>
-            <button onClick={clickHandler(6)}>6</button>
+            <button onClick={() => {
+                clickHandler(4)    
+            }}>4</button>
+            <button onClick={() => {
+                clickHandler(5)    
+            }}>5</button>
+            <button onClick={() => {
+                clickHandler(6)    
+            }}>6</button>
         </div>
         <div>
-            <button onClick={clickHandler(1)}>1</button>
-            <button onClick={clickHandler(2)}>2</button>
-            <button onClick={clickHandler(3)}>3</button>
+            <button onClick={() => {
+                clickHandler(1)    
+            }}>1</button>
+            <button onClick={() => {
+                clickHandler(2)    
+            }}>2</button>
+            <button onClick={() => {
+                clickHandler(3)    
+            }}>3</button>
         </div>
         <div>
-            <button onClick={clickHandler(0)}>0</button>
+            <button onClick={() => {
+                clickHandler(0)    
+            }}>0</button>
             <button>.</button>
         </div>
     </div>
